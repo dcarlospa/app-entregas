f6893d45f2450596dea1cf2db1a8fa58ba839bb8
Diemisom Melo <dcarlospa@gmail.com>
2022-09-15 20:57:58 -0300

    feat: login via firestore - wrong method

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -11,6 +11,7 @@ import { Observable } from 'rxjs';
 })
 export class LoginPage implements OnInit {
 
+  contador = 0;
   email: string = '';
   senha = '';
   produtos : any;
@@ -18,10 +19,14 @@ export class LoginPage implements OnInit {
 
   constructor(
     private router: Router,
-    firestore: AngularFirestore
+    public firestore: AngularFirestore
   ) { 
     console.log(router.url);
-    this.produtos = firestore.collection('produtos').valueChanges();
+    firestore.collection('produtos', ref => ref.limit(10).orderBy('valor', 'desc')).valueChanges().subscribe( x => {
+      this.produtos = x;
+      console.log(x);
+    });
+
     console.log(this.produtos)
   }
 
@@ -31,13 +36,36 @@ export class LoginPage implements OnInit {
   entrar(){
     console.log('entrando...');
     console.log(this.email, this.senha);
+    this.firestore.collection('usuarios', 
+      ref => ref.
+        where('email', '==', this.email).
+        where('senha', '==', this.senha).
+        where('estaAtivo', '==', true).
+        limit(6)
+      ).valueChanges().subscribe( x => {
+        console.log(x);
+        if(x.length === 1){
+          alert('bem vindo')
+          this.router.navigateByUrl('home');
+        }else{
+          //não está logado
+          alert('Você não está logado!');
+          this.router.navigateByUrl('login');
+
+        }
+      })
+    /*
     if(Validate.validateEmail(this.email) && this.senha === this.senhaMestre)
       this.router.navigateByUrl('home');
     else
       alert('Dados incorretos');
+      */
   }
  
 
+  getEmailMaiusculo(){
+    return this.email.toUpperCase();
+  }
   
 
 }
